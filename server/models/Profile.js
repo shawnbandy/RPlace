4bf85ee3eb2c1c31d5826e3ee49272dd3f2d38e0
jmg <macgreene14@gmail.com>
2022-12-13 20:43:20 -0500

    added fields to profile model for storing user settings

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -7,19 +7,64 @@ const graffitiPost = new Schema({
     unique: false,
     trim: true,
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    //get: (timestamp) => dateFormat(timestamp),
+  },
 });
 
 const profileSchema = new Schema({
-  graffitiPosts: [graffitiPost], // array of graffiti posts
-  profileConfig: {
+  // background styling, to be applied to parent component
+  backgroundStyling: {
     type: String,
     unique: false,
     trim: true,
   },
-  // profile config template
-  // color theme
-  // youtube
-  // spotify
+
+  // point to src of profile pic
+  profilePicture: {
+    type: String,
+    unique: false,
+    trim: true,
+  },
+
+  // biography of user
+  aboutMe: {
+    type: String,
+    unique: false,
+    trim: true,
+  },
+
+  // specific qualities to display on page
+  details: {
+    age: {
+      type: String,
+      unique: false,
+      trim: true,
+    },
+    status: {
+      type: String,
+      unique: false,
+      trim: true,
+    },
+  },
+
+  // html w inline styling for media e.g. spotify iframe
+  mediaContainer: {
+    type: String,
+    unique: false,
+    trim: true,
+  },
+
+  // container for iframe of website
+  portfolioContainer: {
+    type: String,
+    unique: false,
+    trim: true,
+  },
+
+  graffitiPosts: [graffitiPost], // array of graffiti posts
 });
 
 const Profile = model("profile", profileSchema);

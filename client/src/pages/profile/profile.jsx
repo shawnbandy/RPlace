431f958bfecae6529c7331d3b935aafc6a0250f1
jmg <macgreene14@gmail.com>
2022-12-15 10:20:33 -0500

    returning profile from backend to frontend

--- client/src/pages/profile/profile.jsx
+++ client/src/pages/profile/profile.jsx
@@ -6,8 +6,13 @@ import ListItemText from '@mui/material/ListItemText';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
 import Paper from '@mui/material/Paper';
+import { useMutation, useQuery } from '@apollo/client';
+import { ME } from '../../context/queries';
+// import { ME } from '../src/context/mutations';
+//client/src/context/queries.js
 
 function LeftProfile(props) {
+
     return  (
         <>
         <Grid container spacing={2}>
@@ -66,18 +71,37 @@ function RightProfile(props) {
     )
 }
 
+function Media(props) {
+    console.log(props.props.me.profile.mediaContainer)
+    return <div dangerouslySetInnerHTML={{__html:props.props.me.profile.mediaContainer}}>
+    </div>
+}
+
+function Widget(props) {
+    console.log(props.props.me.profile.widgetContainer)
+    return <div dangerouslySetInnerHTML={{__html:props.props.me.profile.widgetContainer}}>
+    </div>
+}
+
 export default function ProfilePage() {
+    const { loading, error, data } = useQuery(ME);
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+
     return (
         <>
         <Box sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
                 <Grid xs={12} md={6}>
-                    <LeftProfile />
+                    <LeftProfile props={data} />
                 </Grid>
                 <Grid xs={12} md={6}>
-                    <RightProfile />
+                    <RightProfile props={data}/>
                 </Grid>
             </Grid>
+            <Media props={data}/>
+            <Widget props={data}/>
+
         </Box>
         </>
     )

8269329da6f22765f40f840f1bbbaf052d515fdf
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-12-15 22:08:42 -0500

    some changes

--- client/src/pages/register/register.jsx
+++ client/src/pages/register/register.jsx
@@ -39,7 +39,7 @@ export default function Register() {
       });
 
       AuthService.login(data.addUser.token);
-      navigate('/profile');
+      navigate('/login');
     } catch (err) {
       console.error(err);
     }

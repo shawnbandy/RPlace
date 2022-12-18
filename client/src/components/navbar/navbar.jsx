e4ad3b87d6b6eebf1eab2aa0a6c7ed1a6364f9fe
Corey B <corey.bennett@live.com>
2022-12-18 12:33:39 -0500

    feed page styling, navbar reorienting

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,3 +1,4 @@
+import "./navbar.css";
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -7,70 +8,67 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import { styled, alpha } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
 import InputBase from '@mui/material/InputBase';
 
 // Sets styling for the search bar inside of the Navbar (the example given by Material UI documentation)
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  }));
+// const Search = styled('div')(({ theme }) => ({
+//     position: 'relative',
+//     borderRadius: theme.shape.borderRadius,
+//     backgroundColor: alpha(theme.palette.common.white, 0.15),
+//     '&:hover': {
+//       backgroundColor: alpha(theme.palette.common.white, 0.25),
+//     },
+//     marginLeft: 0,
+//     width: '100%',
+//     [theme.breakpoints.up('sm')]: {
+//       marginLeft: theme.spacing(1),
+//       width: 'auto',
+//     },
+//   }));
   
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
+//   const SearchIconWrapper = styled('div')(({ theme }) => ({
+//     padding: theme.spacing(0, 2),
+//     height: '100%',
+//     position: 'absolute',
+//     pointerEvents: 'none',
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   }));
   
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
-    },
-}));
+//   const StyledInputBase = styled(InputBase)(({ theme }) => ({
+//     color: 'inherit',
+//     '& .MuiInputBase-input': {
+//       padding: theme.spacing(1, 1, 1, 0),
+//       // vertical padding + font size from searchIcon
+//       paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+//       transition: theme.transitions.create('width'),
+//       width: '100%',
+//       [theme.breakpoints.up('sm')]: {
+//         width: '12ch',
+//         '&:focus': {
+//           width: '20ch',
+//         },
+//       },
+//     },
+// }));
 
-const searchBar = (<Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>);
+// const searchBar = (<Search>
+//             <SearchIconWrapper>
+//               <SearchIcon />
+//             </SearchIconWrapper>
+//             <StyledInputBase
+//               placeholder="Search…"
+//               inputProps={{ 'aria-label': 'search' }}
+//             />
+//           </Search>);
 
-const pages = ['Home','Profile','Friends', 'Search',];
-const settings = ['Notifications', 'Settings', 'Signout'];
+const pages = ['Profile','Friends', 'Search', 'Settings', 'Sign Out'];
 
 function NavbarComponent() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -101,166 +99,63 @@ function NavbarComponent() {
 
 
   return (
-    <AppBar style={{"position":"fixed"}}>
-      <Container maxWidth="xl">
+    <AppBar style={{"position":"fixed", "height":"62px", 'background-color': '#560027'}}>
+      <Container maxWidth="x1">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+        <a href="/home" ><img src="/assets/RPlaceIcon.png" alt="RPlaceIcon" className='rplogo'/></a>
           <Typography
             variant="h6"
             noWrap
-            component="a"
-            href="/"
             sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
+              ml:-.5,
+              mr: 3,
+              display: { xs: 'none', sm: 'flex' },
+              fontSize: '1.5rem',
               fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
+              letterSpacing: '.2rem',
               color: 'inherit',
               textDecoration: 'none',
             }}
           >
-            ЯPlace
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={()=>{handleCloseNavMenu();redirectPage(page);}}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            ЯPlace
+            lace
           </Typography>
           
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {/* {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={()=>{handleCloseNavMenu();redirectPage(page);}}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-              
-            ))} */}
+          <Box sx={{ flexGrow: 1, position: 'absolute', right: 0, display: 'flex'}}>
             <Button
                 key={pages[0]}
                 onClick={()=>{handleCloseNavMenu();redirectPage(pages[0]);}}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 1, color: 'white'}}
               >
                 {pages[0]}
             </Button>
             <Button
                 key={pages[1]}
                 onClick={()=>{handleCloseNavMenu();redirectPage(pages[1]);}}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 1, color: 'white'}}
               >
                 {pages[1]}
             </Button>
             <Button
                 key={pages[2]}
                 onClick={()=>{handleCloseNavMenu();redirectPage(pages[2]);}}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 1, color: 'white'}}
               >
                 {pages[2]}
             </Button>
             <Button
-                key={searchBar}
-                onClick={()=>{handleCloseNavMenu();}}
-                onChange={handleSearch}
-                on
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {searchBar}
-            </Button>
-            <Button
                 key={pages[3]}
-                onClick={()=>{handleCloseNavMenu();redirectPage(pages[3]+"/?q="+searchValue);}}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                onClick={()=>{handleCloseNavMenu();redirectPage(pages[3]);}}
+                sx={{ my: 1, color: 'white'}}
               >
                 {pages[3]}
             </Button>
-          </Box>
-          
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={()=>{handleCloseUserMenu();redirectPage(setting);}}>
-                  <Typography textAlign="center">{setting}</Typography>
-                  
-                </MenuItem>
-              ))}
-              
-                
-              
-            </Menu>
+            <Button
+                key={pages[3]}
+                onClick={()=>{handleCloseNavMenu();redirectPage(pages[4]);}}
+                sx={{ my: 1, color: 'white'}}
+              >
+                {pages[4]}
+            </Button>
           </Box>
         </Toolbar>
       </Container>

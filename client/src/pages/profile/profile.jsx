ab05487ce84f628809b1be80ed865efeec9b945d
Hunter O'Neal <hunteroneal54@gmail.com>
2022-12-18 10:46:58 -0600

    fixed syntax, got through my COVID Brain lol

--- client/src/pages/profile/profile.jsx
+++ client/src/pages/profile/profile.jsx
@@ -9,6 +9,10 @@ import Paper from '@mui/material/Paper';
 import { useMutation, useQuery } from '@apollo/client';
 import { ME } from '../../context/queries';
 import { colors } from '@mui/material';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 // import { ME } from '../src/context/mutations';
 import NavbarComponent from "../../components/navbar/navbar";
 
@@ -65,10 +69,56 @@ function LeftProfile(props) {
 function RightProfile(props) {
     return(
         <>
-        
+        <Grid container spacing={2}>
+            <Grid>
+                <iframe title='Profile IFrame' src={props.webPlugin} height={1000} width={500} ></iframe>
+            </Grid>
+            <Grid>
+                <Paper>
+                    <h6>Wall Graffiti!</h6>
+                    <br/>
+                    <List>
+                        <ListItem>{props.graffiti1}</ListItem>
+                        <ListItem>{props.graffiti2}</ListItem>
+                        <ListItem>{props.graffiti3}</ListItem>
+                    </List>
+                </Paper>
+            </Grid>
+        </Grid>
         </>
     )
 }
+
+function Posts(props) {
+    const postContent = props.props.posttext
+    return(
+        <>
+            {postContent.map(postContent => 
+            <>
+                <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                        <Avatar alt="User Image" src={props.props.profilePicture} />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary={props.props.firstName + " " + props.props.lastName}
+                        secondary={<React.Fragment>
+                            <Typography
+                                sx={{ display: 'inline' }}
+                                component="span"
+                                variant="body2"
+                                color="text.primary"
+                            >
+                            </Typography>
+                            {postContent}
+                        </React.Fragment>} />
+                    </ListItem>
+                <Divider variant="inset" component="li" />
+            </>
+            )}
+        </>
+    )
+}
+
 
 function Media(props) {
     return <div dangerouslySetInnerHTML={{__html:props.props.mediaContainer}}>
@@ -86,6 +136,7 @@ export default function ProfilePage() {
     if (error) return `Error! ${error.message}`;
     console.log(data.me.profile)
     const profile = data.me.profile
+    const posts = data.me.posts
     return (
         <div>
         <NavbarComponent />
@@ -100,6 +151,15 @@ export default function ProfilePage() {
             </Grid>
             <Media props={profile}/>
             <Widget props={profile}/>
+        </Box>
+        <Box sx={{ flexGrow: 1 }} style={{"backgroundColor":"blue"}}>
+            <Grid>
+                <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                    <Posts
+                        props = {posts && profile}
+                    />
+                </List>
+            </Grid>
         </Box>
         </div>
     )
